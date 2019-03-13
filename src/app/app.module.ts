625d2ddd2fe03e8fae9b95d60d5fede6ed932ba8
Vadym <vadymko@tahometer.com>
2019-03-13 18:13:27 +0200

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormListComponent } from './form/form-list/form-list.component';
+import { FormTaskComponent } from './form/form-task/form-task.component';
+import { ViewListComponent } from './view/view-list/view-list.component';
+import { ViewTaskComponent } from './view/view-task/view-task.component';
+import { TaskComponent } from './view/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormListComponent,
+    FormTaskComponent,
+    ViewListComponent,
+    ViewTaskComponent,
+    TaskComponent,
   ],
   imports: [
     BrowserModule,
